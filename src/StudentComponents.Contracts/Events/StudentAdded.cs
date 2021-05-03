d10903868e75eccbb275452f99ac8c03645c09d7
bryan howell <howellbm@gmail.com>
2021-05-03 16:46:41 -0700

    Initial checkin
    checkin of working base logic

---
+++ src/StudentComponents.Contracts/Events/StudentAdded.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StudentComponents.Contracts.Events
+{
+    public class StudentAdded
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
